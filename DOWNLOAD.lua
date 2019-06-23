964950f4565240cdaada391c799911c1b67145a2
LionZXY <nikita@kulikof.ru>
2019-06-23 21:12:35 +0300

    Create DOWNLOAD.lua

---
+++ DOWNLOAD.lua
@@ -0,0 +1,10 @@
+mkdir /scripts/modules/
+
+wget 'https://raw.githubusercontent.com/LionZXY/OpenComputersMiner/master/HEADER.lua' /scripts/modules/HEADER.lua
+wget 'https://raw.githubusercontent.com/LionZXY/OpenComputersMiner/master/INVENTORY.lua' /scripts/modules/INVENTORY.lua
+wget 'https://raw.githubusercontent.com/LionZXY/OpenComputersMiner/master/MOVEMENT.lua' /scripts/modules/MOVEMENT.lua
+wget 'https://raw.githubusercontent.com/LionZXY/OpenComputersMiner/master/ZIGZAG.lua' /scripts/modules/ZIGZAG.lua
+wget 'https://raw.githubusercontent.com/LionZXY/OpenComputersMiner/master/PLAIN.lua' /scripts/modules/PLAIN.lua
+wget 'https://raw.githubusercontent.com/LionZXY/OpenComputersMiner/master/SQUARE.lua' /scripts/modules/SQUARE.lua
+wget 'https://raw.githubusercontent.com/LionZXY/OpenComputersMiner/master/MAIN.lua' /scripts/modules/MAIN.lua
+wget 'https://raw.githubusercontent.com/LionZXY/OpenComputersMiner/master/autorun.lua' /autorun.lua
